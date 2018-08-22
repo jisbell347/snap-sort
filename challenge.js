03ad3178d5da04d632f65d9249924300829433dc
Joseph Isbell <shadowwolf347@comcast.net>
2018-08-22 09:06:44 -0600

    Added code that would turn the string into an array, sort it and remove the word sensor

---
+++ challenge.js
@@ -0,0 +1,21 @@
+const str = "It indicates a synchronic distortion in the areas emanating triolic waves. The cerebellum, the cerebral" +
+	" cortex, the brain stem,  the entire nervous system has been depleted of electrochemical energy. Any device like that " +
+	"would produce high levels of triolic waves. These walls have undergone some kind of selective molecular polarization. " +
+	"I haven't determined if our phaser energy can generate a stable field. We could alter the photons with phase discriminators. " +
+	"Sensors indicate human life forms 30 meters below the planet's surface. Stellar flares are increasing in magnitude and frequency. " +
+	"Set course for Rhomboid Dronegar 006, warp seven. There's no evidence of an advanced communication network. Total guidance system failure, " +
+	"with less than 24 hours' reserve power. Shield effectiveness has been reduced 12 percent. We have covered the area in a spherical pattern which a " +
+	"ship without warp drive could cross in the given time. Resistance is futile.";
+
+let words = str.toLowerCase().split(' ');
+words.sort(function(a, b) {
+	let c = a.toLowerCase();
+	let d = b.toLowerCase();
+	return(c < d) ? -1 : (c > d) ? 1 : 0;
+});
+
+let newWords = words.filter(word => {
+	return !word.includes("sensor");
+})
+
+console.log(newWords.join(' '));
